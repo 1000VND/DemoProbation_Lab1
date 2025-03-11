4f6fc5569771cc41cc5653c40dfc269ec851acd3
hunglq <alatso57@gmail.com>
2025-03-11 14:32:56 +0700

    create folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './router/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { AuthGuard } from './guards/auth.guard';
-import { PersonnelManagementComponent } from './router/personnel-management/personnel-management.component';
+import { PersonnelManagementComponent } from './components/personnel-management/personnel-management.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' }, // Chuyển hướng mặc định đến login
